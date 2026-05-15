d92b4f2a60afd245bcb26a1b79d46286cee986e9
GuilhermeBarbosa759 <g294266@dac.unicamp.br>
2026-05-14 21:20:13 -0300

    ajustes para deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
+  output: 'export',
+  basePath: '/AuraBank',
+  assetPrefix: '/AuraBank', 
   images: {
     unoptimized: true,
   }
