bf76f3af4d08af8afe3a432803aeb37681156ded
GuilhermeBarbosa759 <g294266@dac.unicamp.br>
2026-05-14 21:39:02 -0300

    removendo configurações que quebram o vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/MEU-BANCO-APP',
-  assetPrefix: '/MEU-BANCO-APP', 
   images: {
     unoptimized: true,
-  }
+  },
 };
 
 export default nextConfig;
